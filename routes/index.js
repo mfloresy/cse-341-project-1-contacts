07c214e6b34593467caf60c174ebc92b6e3bedd0
Manuel Flores <mfloresyanez@byupathway.edu>
2025-05-20 22:09:28 -0400

    added main route again

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+router.get("/", (req, res) => {
+    res.send("Main page")
+})
+
 router.use("/contacts", require("./contacts"))
 
 module.exports = router;
